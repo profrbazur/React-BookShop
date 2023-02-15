8634f7d73be421675fe7f470a423b911a0b99cd2
profrbazurph <profrbazurph@gmail.com>
2023-02-15 16:43:07 +0800

    Done

--- src_2/pages/Order/OrderPage.js
+++
@@ -1,5 +0,0 @@
-export const OrderPage = () => {
-  return (
-    <div>OrderPage</div>
-  )
-}
--- src_2/pages/index.js
+++
@@ -1,6 +0,0 @@
-export { HomePage } from './Home/HomePage';
-export { ProductsList } from './Products/ProductsList';
-export { ProductDetail } from './ProductDetail';
-
-export { Login } from './Login';
-export { Register } from './Register';
