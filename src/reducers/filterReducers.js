fb714ba805d8a01f7cb17cb2073eb8652be678fb
profrbazurph <profrbazurph@gmail.com>
2023-02-15 14:30:03 +0800

    Clear Filter

--- src/reducers/filterReducers.js
+++ src/reducers/filterReducers.js
@@ -18,9 +18,15 @@ export const filterReducer = (state, action) => {
       return { ...state, onlyInStock: payload.onlyInStock };
 
     case 'CLEAR_FILTER':
-      return;
+      return {
+        ...state,
+        onlyInStock: false,
+        bestSellerOnly: false,
+        sortBy: null,
+        ratings: null,
+      };
 
     default:
-      throw new Error('No Cae Found!');
+      throw new Error('No Case Found!');
   }
 };
