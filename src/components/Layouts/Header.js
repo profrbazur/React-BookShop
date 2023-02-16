6b354f3b312c6a773dfd20e441f7529f7d21d845
profrbazurph <profrbazurph@gmail.com>
2023-02-16 15:45:10 +0800

    Done

--- src/components/Layouts/Header.js
+++ src/components/Layouts/Header.js
@@ -3,8 +3,10 @@ import { Link } from 'react-router-dom';
 import Logo from '../../assets/logo.png';
 import { Search } from '../Sections/Search';
 import { DropdownLoggedOut, DropdownLoggedIn } from '../index';
+import { useCart } from '../../context';
 
 export const Header = () => {
+  const { cartList } = useCart();
   const [darkMode, setDarkMode] = useState(
     JSON.parse(localStorage.getItem('darkMode')) || false
   );
@@ -44,7 +46,7 @@ export const Header = () => {
             <Link to="/cart" className="text-gray-700 dark:text-white mr-5">
               <span className="text-2xl bi bi-cart-fill relative">
                 <span className="text-white text-sm absolute -top-1 left-2.5 bg-rose-500 px-1 rounded-full ">
-                  0
+                  {cartList.length}
                 </span>
               </span>
             </Link>
