44f58609a83a0981cad8cc5951e45fc88dc32b64
profrbazurph <profrbazurph@gmail.com>
2023-02-16 16:05:32 +0800

    Done

--- src/components/Elements/ProductCard.js
+++ src/components/Elements/ProductCard.js
@@ -1,9 +1,12 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useCart } from '../../context';
 import { Rating } from './Rating';
 
 export const ProductCard = ({ product }) => {
   const { cartList, addToCart, removeFromCart } = useCart();
+  const [inCart, setInCart] = useState(false);
   const {
     id,
     name,
@@ -15,9 +18,15 @@ export const ProductCard = ({ product }) => {
     best_seller,
   } = product;
 
-  function handleClick(product) {
-    addToCart(product);
-  }
+  useEffect(() => {
+    const productInCart = cartList.find((item) => item.id === product.id);
+
+    if (productInCart) {
+      setInCart(true);
+    } else {
+      setInCart(false);
+    }
+  }, [cartList, product.id]);
 
   return (
     <div className="m-3 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
@@ -52,13 +61,22 @@ export const ProductCard = ({ product }) => {
             <span>$</span>
             <span>{price}</span>
           </span>
-          <button
-            onClick={() => handleClick(product)}
-            className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800"
-          >
-            Add To Cart <i className="ml-1 bi bi-plus-lg"></i>
-          </button>
-          {/* <button className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-800">Remove Item <i className="ml-1 bi bi-trash3"></i></button> */}
+          {!inCart && (
+            <button
+              onClick={() => addToCart(product)}
+              className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800"
+            >
+              Add To Cart <i className="ml-1 bi bi-plus-lg"></i>
+            </button>
+          )}
+          {inCart && (
+            <button
+              onClick={() => removeFromCart(product)}
+              className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-800"
+            >
+              Remove Item <i className="ml-1 bi bi-trash3"></i>
+            </button>
+          )}
         </p>
       </div>
     </div>
