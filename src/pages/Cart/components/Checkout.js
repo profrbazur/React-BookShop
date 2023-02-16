59ff7d0eb8d1f6de1b660b316921a1831e81063c
profrbazurph <profrbazurph@gmail.com>
2023-02-17 06:51:31 +0800

    Done

--- src/pages/Cart/components/Checkout.js
+++ src/pages/Cart/components/Checkout.js
@@ -1,15 +1,17 @@
-import { useState } from 'react';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useCart } from '../../../context';
 
 export const Checkout = ({ setCheckout }) => {
-  const { total } = useCart();
+  const { cartList, total, clearCart } = useCart();
   const [user, setUser] = useState({});
 
-  useEffect(() => {
-    const token = JSON.parse(sessionStorage.getItem('token'));
-    const cbid = JSON.parse(sessionStorage.getItem('cbid'));
+  const navigate = useNavigate();
 
+  const token = JSON.parse(sessionStorage.getItem('token'));
+  const cbid = JSON.parse(sessionStorage.getItem('cbid'));
+
+  useEffect(() => {
     async function getUser() {
       const response = await fetch(`http://localhost:8000/600/users/${cbid}`, {
         method: 'GET',
@@ -21,9 +23,35 @@ export const Checkout = ({ setCheckout }) => {
       const data = await response.json();
       setUser(data);
     }
-
     getUser();
   }, []);
+
+  async function handleOrderSubmit(event) {
+    event.preventDefault();
+
+    const order = {
+      cartList: cartList,
+      amount_paid: total,
+      quantity: cartList.length,
+      user: {
+        name: user.name,
+        email: user.email,
+        id: user.id,
+      },
+    };
+
+    const response = await fetch('http://localhost:8000/660/orders', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(order),
+    });
+    const data = await response.json();
+    clearCart();
+    navigate('/');
+  }
 
   return (
     <section>
@@ -62,7 +90,7 @@ export const Checkout = ({ setCheckout }) => {
               <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">
                 <i className="bi bi-credit-card mr-2"></i>CARD PAYMENT
               </h3>
-              <form className="space-y-6">
+              <form onSubmit={handleOrderSubmit} className="space-y-6">
                 <div>
                   <label
                     htmlFor="name"
@@ -92,7 +120,7 @@ export const Checkout = ({ setCheckout }) => {
                     name="email"
                     id="email"
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:value-gray-400 dark:text-white"
-                    value={user.email || 'default@email.com'}
+                    value={user.email || 'backup@example.com'}
                     disabled
                     required=""
                   />
