d5261c87ea4079ec525867536c4640c3121e1707
profrbazurph <profrbazurph@gmail.com>
2023-02-16 18:29:18 +0800

    Done

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -77,7 +77,10 @@ export const ProductDetail = () => {
               {!inCart && (
                 <button
                   onClick={() => addToCart(product)}
-                  className={`inline-flex items-center py-2 px-5 text-lg font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800`}
+                  className={`inline-flex items-center py-2 px-5 text-lg font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 ${
+                    product.in_stock ? '' : 'cursor-not-allowed'
+                  }`}
+                  disabled={product.in_stock ? '' : 'disabled'}
                 >
                   Add To Cart <i className="ml-1 bi bi-plus-lg"></i>
                 </button>
@@ -85,7 +88,9 @@ export const ProductDetail = () => {
               {inCart && (
                 <button
                   onClick={() => removeFromCart(product)}
-                  className={`inline-flex items-center py-2 px-5 text-lg font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-800`}
+                  className={`inline-flex items-center py-2 px-5 text-lg font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-800 ${
+                    product.in_stock ? '' : 'cursor-not-allowed'
+                  }`}
                   disabled={product.in_stock ? '' : 'disabled'}
                 >
                   Remove Item <i className="ml-1 bi bi-trash3"></i>
