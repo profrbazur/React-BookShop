cef29cace9ad306171bb0a6a6144ee0463b5bb32
Ryan Azur <33971864+profrbazur@users.noreply.github.com>
2023-02-20 11:37:42 +0800

    Update Register.js
    Done

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -49,7 +49,7 @@ export const Register = () => {
             type="name"
             id="name"
             className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-            placeholder="Shubham Sarda"
+            placeholder="Your name"
             required
             autoComplete="off"
           />
@@ -65,7 +65,7 @@ export const Register = () => {
             type="email"
             id="email"
             className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-            placeholder="shubham@example.com"
+            placeholder="youremail@email.com"
             required
             autoComplete="off"
           />
